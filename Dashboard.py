8b1aa8e6fb452f2c8e10f7e640e6032868f2dfee
Dashboard-yts-sys <yantasjatim@gmail.com>
2026-05-09 05:53:53 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -189,7 +189,7 @@ mode_periode = st.sidebar.selectbox(
 pilih_bulan = st.sidebar.selectbox(
     "Pilih Bulan",
     list(bulan_map.keys()),
-    index=2
+    index=3
 )
 
 kode_bulan = bulan_map[pilih_bulan]
